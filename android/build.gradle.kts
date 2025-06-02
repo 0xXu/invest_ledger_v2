f201f2a11866c3f237c5e351d6a60b7344dc7634
0xXu <huangxucsdm@gmail.com>
2025-06-02 17:32:55 +0800

    Fix:修复bug

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,6 +7,21 @@ allprojects {
     }
 }
 
+// 全局配置Java版本，避免Java 8过时警告
+subprojects {
+    tasks.withType<JavaCompile> {
+        options.compilerArgs.addAll(listOf("-Xlint:-options"))
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+}
+
 
 // subprojects {
 //     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
